3474c5c8d00e2f803e68bd1e298a1475e44eeb8e
ahob85 <aaron@ahob85.com>
2017-10-07 10:52:59 -0700

    added intro and play again

--- cs3a/AvoiderGameTeach/AvoiderGameIntroWorld.java
+++ cs3a/AvoiderGameTeach/AvoiderGameIntroWorld.java
@@ -1,19 +1,19 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class AvoiderGameOverWorld here.
+ * Write a description of class AvoiderGameIntroWorld here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class AvoiderGameOverWorld extends World
+public class AvoiderGameIntroWorld extends World
 {
 
     /**
-     * Constructor for objects of class AvoiderGameOverWorld.
+     * Constructor for objects of class AvoiderGameIntroWorld.
      * 
      */
-    public AvoiderGameOverWorld()
+    public AvoiderGameIntroWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
