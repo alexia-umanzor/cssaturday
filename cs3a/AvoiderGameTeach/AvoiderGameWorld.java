0c1a298ee184d7beae509a271df0217c482b736c
ahob85 <aaron@ahob85.com>
2017-10-14 10:31:58 -0700

    update

--- cs3a/AvoiderGameTeach/AvoiderGameWorld.java
+++ cs3a/AvoiderGameTeach/AvoiderGameWorld.java
@@ -21,8 +21,7 @@ public class AvoiderGameWorld extends World
         super(600, 400, 1, false); 
         setAvatar(); 
         bgmusic = new GreenfootSound("sounds/clotho.mp3");
-        //bgmusic.playLoop();
-        
+        bgmusic.playLoop();
     }
     
     public void act() {
