d135c7493eda377ee791ebd1af87b5fe0974f102
ahob85 <aaron@ahob85.com>
2017-10-14 10:25:36 -0700

    Add print statement to AvoiderGameWorld

--- cs3a/AvoiderGameTeach/AvoiderGameWorld.java
+++ cs3a/AvoiderGameTeach/AvoiderGameWorld.java
@@ -22,6 +22,7 @@ public class AvoiderGameWorld extends World
         setAvatar(); 
         bgmusic = new GreenfootSound("sounds/clotho.mp3");
         bgmusic.playLoop();
+        
     }
     
     public void act() {
