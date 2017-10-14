4c5741a2a562da7e9e5716e53f3b3d2bcbfb957e
ahob85 <aaron@ahob85.com>
2017-10-14 12:20:51 -0700

    update

--- cs3a/AvoiderGameTeach/Enemy.java
+++ cs3a/AvoiderGameTeach/Enemy.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Enemy extends Actor
 {
-    //Instructor
+    //Instance Variable
     private int speed;
    
     /**
