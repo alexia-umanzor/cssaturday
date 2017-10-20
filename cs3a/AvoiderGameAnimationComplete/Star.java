4cf543da1aa458d17d6299c14a3b7bcc263a14a7
Aaron Hobson <aaron@ahob85.com>
2017-10-19 18:04:12 -0700

    update

--- cs3a/AvoiderGameAnimationComplete/Star.java
+++ cs3a/AvoiderGameAnimationComplete/Star.java
@@ -9,6 +9,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Star extends Actor
 {
     private int speed = 1;
+    private int twinkleTime = 0;
+    private int currentTransparency = 0;
     
     public Star() {
         GreenfootImage img = new GreenfootImage(10, 10);
@@ -25,12 +27,30 @@ public class Star extends Actor
     {
         setLocation(getX(), getY() + speed);
         checkRemove();
+        checkTwinkle();
     }    
     
     private void checkRemove() {
         World w = getWorld();
         if( getY() > w.getHeight() + 30) {
             w.removeObject(this);
+        }
+    }
+    
+    private void checkTwinkle() {
+        GreenfootImage img = getImage();
+        if(twinkleTime > 0) {
+            if(twinkleTime == 1) {
+                img.setTransparency(currentTransparency);
+            }
+            twinkleTime--;
+        }
+        else {
+            if(Greenfoot.getRandomNumber(10000) < 10) {
+                twinkleTime = 10;
+                currentTransparency = img.getTransparency();
+                img.setTransparency(0);
+            } 
         }
     }
     
